3537de1804d4283ddb47bff2eb4283c99be7f7c4
Hore01 <akinremiolajumoke01@gmail.com>
2022-07-01 17:18:23 -0700

    All as of now Emarv

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,32 @@
-#include <stdlib.h> 
- #include <time.h> 
- #include <stdio.h> 
- /** 
-  *main - main block 
-  *Description: Print statements based on the last digit of the random number. 
-  *Return: 0
- */ 
- int main(void)
- {
-      int n;
-      int m;
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-      srand(time(0));
-      n = rand () - RAND_MAX / 2;
-      m = n % 10;
+/**
+ * main - Prints a text according number
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n, lastd;
 
-     if (m > 5)
-	     printf("Last digit of %d and is greater than 5\n", n, m);
-     else if (m == 0)
-	     printf("Last digit of %d and is 0\n", n, m);
-     else
-	     printf("Last digit %d and is less than 6 and not 0\n", n,m);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
 
-     return (0);
- }
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+
+	return (0);
+}
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
- * 
- * Return: Always 0.
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
  */
 int main(void)
 {
-		char letter;
+	char c;
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-		     putchar(letter);
-		for (letter = 'A'; letter <= 'z'; letter++)
-		     putchar(letter);
-		     putchar('\n');
-		     return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
