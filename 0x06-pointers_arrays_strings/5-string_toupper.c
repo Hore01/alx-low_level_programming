dbcfa38ebffa8a66db848753b471763bcffffc0a
Hore01 <akinremiolajumoke01@gmail.com>
2022-07-14 05:18:04 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,25 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * *cap_string - this is awesome
- * @s: pointer to char params
- *
- * Return: *s
- */
+*string_toupper - changes all lowercases letters of a string to uppercase
+*@str: parameter
+*Return: returns a character
+*/
 
-char *cap_string(char *s)
+char *string_toupper(char *str)
 {
-	int i, j;
-	char delimeters[] = " \t\n,;.!?\"(){}";
+	int index = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (str[index++])
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-			s[0] = s[0] - 32;
-				for (j = 0; delimeters[j] != '\0'; j++)
-					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-						s[i + 1] = s[i + 1] - 32;
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
 	}
-	return (s);
+
+	return (str);
 }
